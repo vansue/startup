a2ba1ce642e92157edd54a07956288a0e9647c3a
vansue <trongnghiahp85@yahoo.com>
2013-05-15 15:24:44 +0700

    startup a new project
    Created on 15/05/2013

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset='UTF-8' />
+	<title>Start up</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</head>
+
+<body>
+</body>
+
+</html>
