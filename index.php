d9d9fd704cc08fd8e438ea0585b633fab315b783
vansue <trongnghiahp85@yahoo.com>
2013-05-15 15:27:33 +0700

    Bo sung them vao file index.php

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
 	<meta charset='UTF-8' />
 	<title>Start up</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<!--<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>-->
+	<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="js/example.js"></script>
 </head>
 
 <body>
